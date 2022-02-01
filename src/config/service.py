97e59955e8c248bd5443457c7ce2a5385b68b01d
rsaleev <rs.aleev@gmail.com>
2022-02-01 21:15:02 +0300

    исправлен приоритет загрузки настроек

--- src/config/service.py
+++ src/config/service.py
@@ -9,24 +9,24 @@ https://pydantic-docs.helpmanual.io/usage/settings/
 class Settings(BaseSettings):
 
     fastapi_app_debug: bool = True
-    fastapi_app_title:Optional[str] = 'FastAPI'
+    fastapi_app_title:Optional[str] = 'Справочники'
     fastapi_app_version:Optional[str] = '0.1.0'
-    cors_allow_methods: Optional[List[str]] = ["*"]
+    cors_allow_methods: Optional[List[str]] = ["POST", "DELETE", "GET", "PUT"]
     cors_allow_origins: Optional[List[str]] = ["*"]
     cors_allow_credentials: Optional[bool] = True
     cors_allow_headers: Optional[List[str]]= ["*"]
 
     class Config:
-        env_file_encoding = 'utf-8'
 
         @classmethod
         def customise_sources(
             cls,
             env_settings,
             init_settings,
-            
+            file_secret_settings,
         ):
             return (
                 env_settings,
                 init_settings,
+                file_secret_settings
             )
