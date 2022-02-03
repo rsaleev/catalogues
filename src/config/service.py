6801a60d3dd15d11ecaabbb298b86e2028dc7c62
rsaleev <rs.aleev@gmail.com>
2022-02-03 13:45:28 +0300

    исправлена конфигурация

--- src/config/service.py
+++ src/config/service.py
@@ -8,9 +8,10 @@ https://pydantic-docs.helpmanual.io/usage/settings/
 
 class Settings(BaseSettings):
 
-    fastapi_app_debug: bool = True
+    fastapi_app_debug: bool = False
     fastapi_app_title:Optional[str] = 'Справочники'
     fastapi_app_version:Optional[str] = '0.1.0'
+    fastapi_app_author:Dict[str, str] = {'author':'Rostislav Aleev', 'mail':'rs.aleev@gmail.com'}
     cors_allow_methods: Optional[List[str]] = ["POST", "DELETE", "GET", "PUT"]
     cors_allow_origins: Optional[List[str]] = ["*"]
     cors_allow_credentials: Optional[bool] = True
